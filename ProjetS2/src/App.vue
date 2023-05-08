96483f8d3642c2e9eac0f1d26434851af1f2c997
JulesHash <dupaquier.jules@gmail.com>
2023-05-08 19:57:13 +0200

    first commit

---
+++ ProjetS2/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+/*
+import FooterPage from "../src/components/FooterPage.vue"
+*/
+
+</script>
+
+<template>
+  <main>Le contenu principal</main>
+  <FooterPage/>
+</template>
---
+++ ProjetS2/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src//*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+
+    extend: {
+      spacing: {
+
+      },
+      borderRadius: {
+
+      }
+    }
+  },
+  plugins: []
+}
