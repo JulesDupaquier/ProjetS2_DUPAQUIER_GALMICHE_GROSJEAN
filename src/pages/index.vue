fc5470c69d880aaecc04e8e32351644b351600d1
maelonetheflux <mael.grosjean@edu.univ-fcomte.fr>
2023-05-27 19:29:45 +0200

    comit remise en route/ erreur postcss

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,6 +4,8 @@
 
 <template>
     <p>bonjour</p>
-        <h1>Bonjour salut hello hi</h1>
+        <h1>arimo</h1>
         <h2>Bonjour salut hello hi</h2>
+
+        
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,70 +1,63 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src//*.{vue,js,ts,jsx,tsx}'],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    
     colors: {
-  'Primary/100': '#ffe9bf',
-  'Primary/200': '#ffda93',
-  'Primary/300': '#ffcb67',
-  'Primary/400': '#ffbc3b',
-  'Primary/500': '#dd9f28',
-  'Primary/600': '#bb8318',
-  'Primary/700': '#99690c',
-  'Primary/800': '#774f03',
-  'Primary/900': '#553800',
-  'Secondary/100': '#148d44',
-  'Secondary/500': '#1a5e08',
-  'Greyscale/10': '#f5f5f5',
-  'Greyscale/30': '#cccccc',
-  'Greyscale/50': '#a3a3a3',
-  'Greyscale/70': '#7a7a7a',
-  'Greyscale/90': '#3d3d3d',
-  'Greyscale/100': '#000000'
- },
+      'Primary/100': '#ffe9bf',
+      'Primary/200': '#ffda93',
+      'Primary/300': '#ffcb67',
+      'Primary/400': '#ffbc3b',
+      'Primary/500': '#dd9f28',
+      'Primary/600': '#bb8318',
+      'Primary/700': '#99690c',
+      'Primary/800': '#774f03',
+      'Primary/900': '#553800',
+      'Secondary/100': '#148d44',
+      'Secondary/500': '#1a5e08',
+      'Greyscale/10': '#f5f5f5',
+      'Greyscale/30': '#cccccc',
+      'Greyscale/50': '#a3a3a3',
+      'Greyscale/70': '#7a7a7a',
+      'Greyscale/90': '#3d3d3d',
+      'Greyscale/100': '#000000'
+    },
 
- fontSize: {
-  'xxs': '0.9rem',
-  'xs': '1rem',
-  'sm': '1.25rem',
-  'xl': '1.563rem',
-  '2xl': '2.5rem'
-  
- },
+    fontSize: {
+      'xxs': ['0.9rem'],
+      'xs': ['1rem'],
+      'sm': ['1.25rem'],
+      'xl': ['1.563rem'],
+      '2xl': ['2.5rem']
+      
+     },  
 
-  fontFamily: {
-    sans: ['Arimo', 'sans-serif']
-  },
+    fontFamily: {
+      arimo: ['Arimo', 'sans-serif']
 
-borderRadius: {
-  'none': '0',
-  'xs': '0.1875rem',
-  'sm': '0.25rem',
-  'default': '0.3125rem',
-  'lg': '0.375rem',
-  'xl': '0.5rem',
-  '2xl': '0.625rem',
-  '3xl': '1rem',
-  '4xl': '1.5625rem',
-  '5xl': '3.3621575832366943rem',
-  '6xl': '3.7897727489471436rem',
-  '7xl': '4.670040130615234rem',
-  'full': '9999px'
- },
+    },
+    
+  borderRadius: {
+    'none': '0',
+    'xs': '0.1875rem',
+    'sm': '0.25rem',
+    'default': '0.3125rem',
+    'lg': '0.375rem',
+    'xl': '0.5rem',
+    '2xl': '0.625rem',
+    '3xl': '1rem',
+    '4xl': '1.5625rem',
+    '5xl': '3.3621575832366943rem',
+    '6xl': '3.7897727489471436rem',
+    '7xl': '4.670040130615234rem',
+    'full': '9999px'
+   },
+
+
     extend: {
-
       gridTemplateColumns: {
         principal: 'repeat(12, minmax(0, 64px))'
-      },
-      
-      spacing: {
-
-      },
-      borderRadius: {
-
       }
-    }
+		}
   },
-
   plugins: []
 }
