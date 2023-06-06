6c63a2579f39a169e5c1114a29b4b973ad43d5ab
maelonetheflux <mael.grosjean@edu.univ-fcomte.fr>
2023-06-06 09:24:32 +0200

    ajout card tendance

--- src/components/templatetendance.vue
+++ src/components/templatetendance.vue
@@ -1,19 +1,31 @@
 <script setup lang="ts">
+import icon_arrow from '@/components/icons/icon_arrow.vue';
+import { pb } from '@/backend'
+import type { RecetteResponse } from '@/pocketbase-type'
+const props: RecetteResponse = defineProps<RecetteResponse>()
 
+const img1= props.photo
+const urlImg1 = pb.files.getUrl(props, img1, {thumb : 'LxH'})
 </script>
+
 
 <template>
 
 <div class="">
-    <div class="w-[1074px] h-[279px] rounded-tr-[30px] rounded-bl-[30px] bg-greyscale-10 lg:w-[327px] lg:h-[279px] lg:rounded-tr-[30px] lg:rounded-bl-[30px]"></div>
+    <div class="relative z-10 w-[1074px] h-[279px] rounded-tr-[30px] rounded-bl-[30px] bg-greyscale-10 lg:w-[327px] lg:h-[279px] lg:rounded-tr-[30px] lg:rounded-bl-[30px]"></div>
     <p class="w-[977px] h-[59px] text-sm font-bold text-left text-greyscale-10 lg:w-[245px] lg:h-[64px]">{{ nom_recette }}</p>
-    <div class="flex justify-end">
-    <div  class="w-[62px] h-[46.83px] rounded-tl-[20px] bg-primary/400 lg:"></div>
+    <div class=""></div>
+    <img :src="urlImg1" class="w-[1073.77px] h-[215px] rounded-tr-[22.52px] rounded-bl-[22.52px] object-cover lg:w-[327.06px] lg:h-[215px] lg:">
+    
+    <div>
+
+    <div class="relative flex justify-end z-20">
+    <div  class="w-[62px] h-[46.83px] rounded-tl-[20px] bg-primary-400 lg:"></div>
     </div>
 
-
+    <div class="relative flex justify-end z-30"><icon_arrow/></div>
     
-    
+    </div>
 
 </div>
 </template>
